2511129fef4ae0f64deb0970e444b034efc63774
Leandro <leandrowebmaster@gmail.com>
2025-03-03 17:33:05 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -301,10 +301,13 @@ stls.alignItems({ pass: { arrayValue: [jsvals.j8({pass: {
         }})] }}), 
 stls.width({ pass: { arrayValue: [jsvals.j8({pass: {
           propertieValues: "300px"
-        }})] }}), stls.backgroundColor({ pass: { arrayValue: [jsvals.varReader({pass: {
+        }})] }}), 
+stls.backgroundColor({ pass: { arrayValue: [jsvals.varReader({pass: {
           path: jsvals.j8({pass: {
           propertieValues: "all.colors.navigation"
         }})
+        }})] }}), stls.alignSelf({ pass: { arrayValue: [jsvals.j8({pass: {
+          propertieValues: "center"
         }})] }})],
 
           childrenItems: [(...args:any) => <Elements.Text pass={{
