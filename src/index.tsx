7138feb7182c4b8d7d5c73d9e96170a6316d8438
Leandro <leandrowebmaster@gmail.com>
2025-03-07 14:18:39 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -384,6 +384,7 @@ stls.padding({ pass: { arrayValue: [jsvals.j8({pass: {
 
           itemElements: [
             
+        
 
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:[{}],
@@ -417,6 +418,29 @@ stls.padding({ pass: { arrayValue: [jsvals.j8({pass: {
           args,
 
         }}/>],
+
+            args,
+          }}/>
+        , 
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:[{}],
+
+            styles:[
+        stls.width({ pass: { arrayValue: [jsvals.j8({pass: {
+          propertieValues: "90%"
+        }})] }}), 
+        stls.height({ pass: { arrayValue: [jsvals.j8({pass: {
+          propertieValues: "1px"
+        }})] }}), stls.backgroundColor({ pass: { arrayValue: [jsvals.j8({pass: {
+          propertieValues: "black"
+        }})] }})],
+
+            variablePath:[ ],
+
+            expectedVal:[ ],
+
+            childrenItems:[() =><></>],
 
             args,
           }}/>
