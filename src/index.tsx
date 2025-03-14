687980172d9bd85b544e85b46601127556e84cd9
Leandro <leandrowebmaster@gmail.com>
2025-03-14 13:30:41 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -324,9 +324,6 @@ stls.alignItems({ pass: { arrayValue: [jsvals.j8({pass: {
         }})] }}), stls.justifyContent({ pass: { arrayValue: [jsvals.j8({pass: {
           propertieValues: "center"
         }})] }}), 
-stls.width({ pass: { arrayValue: [jsvals.j8({pass: {
-          propertieValues: "500px"
-        }})] }}), 
 stls.backgroundColor({ pass: { arrayValue: [jsvals.varReader({pass: {
           path: jsvals.j8({pass: {
           propertieValues: "all.colors.navigation"
