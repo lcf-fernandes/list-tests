31fb2f188f87d929c715b169b507460dcd706248
Leandro <leandrowebmaster@gmail.com>
2025-03-14 12:58:59 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -425,14 +425,7 @@ stls.padding({ pass: { arrayValue: [jsvals.j8({pass: {
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:[{}],
 
-            styles:[
-              {
-                backgroundColor: 'white',
-                justifyContent: 'center',
-                minHeight: 22,
-                width: "100%",
-              }
-              ],
+            styles:[[]],
 
             variablePath:[ ],
 
