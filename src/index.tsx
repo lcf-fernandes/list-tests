86ecd5667ad0b463b3316e4466ff224ce349cb9f
Leandro <leandrowebmaster@gmail.com>
2025-04-04 14:10:15 -0400

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -99,7 +99,24 @@ stls.alignItems({ pass: { arrayValue: [jsvals.j8({pass: {
               }
               ],
 
-          childrenItems: [()=><></>],
+          childrenItems: [(...args:any) => <Elements.Text pass={{
+          arrProps: [
+            {}
+          ],
+
+          arrStyles: [
+            { color: 'black', fontSize: 12, }
+          ],
+
+          children: [
+            jsvals.j8({pass: {
+          propertieValues: "RRR"
+        }})
+          ],
+
+          args,
+
+        }}/>],
 
           pressableFunctions: [
         (...args) => {
